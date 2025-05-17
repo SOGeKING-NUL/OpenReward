9ea05f229f360be2e5777961c36899facffcd398
sogeking-nul <utsav.jana.5@gmail.com>
2025-05-17 22:35:41 +0530

    updated provider

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -3,14 +3,13 @@
 import { FC, PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CivicAuthProvider } from "@civic/auth-web3/nextjs";
-import { polygonAmoy, baseSepolia, sepolia, xdcTestnet } from "viem/chains";
+import { baseSepolia, sepolia, mainnet } from "viem/chains"; //does not suport base mainnet yet so will have to add it manually
 import { Chain, http } from "viem";
 import { createConfig, WagmiProvider } from "wagmi";
 import { embeddedWallet } from "@civic/auth-web3/wagmi";
 
 const queryClient = new QueryClient();
 
-// Configure chains and RPC URLs.
 export const supportedChains = [sepolia] as [
   Chain,
   ...Chain[],
@@ -25,7 +24,6 @@ const wagmiConfig = createConfig({
   ssr: true,
 });
 
-// Add this type for the Providers props
 type ProvidersProps = PropsWithChildren<{
   onSessionEnd?: () => void;
 }>;
