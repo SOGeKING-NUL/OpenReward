1b21348e3b4466b356a1dc9629da55e43e38d846
sogeking-nul <utsav.jana.5@gmail.com>
2025-05-18 11:05:22 +0530

    fixes model space

--- models/User.ts
+++ models/User.ts
@@ -1,143 +1,143 @@
-    import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
-    const userSchema = new Schema({
-        walletAddress: {
-            type: String,
-            required: true,
-            unique: true,
-        }, 
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        }, 
-        name: {
-            type: String,
-            required: true
-        }, 
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        }, 
-        profilePicture: {
-            type: String,
-            default: null
-        }, 
-        bio: {
-            type: String,
-            default: null
-        }, 
+const userSchema = new Schema({
+    walletAddress: {
+        type: String,
+        required: true,
+        unique: true,
+    }, 
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    }, 
+    name: {
+        type: String,
+        required: true
+    }, 
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    }, 
+    profilePicture: {
+        type: String,
+        default: null
+    }, 
+    bio: {
+        type: String,
+        default: null
+    }, 
 
-        githubUsername: {
-            type: String,
-            index: true,
-            sparse: true
-        }, 
-        githubId: {
-            type: String,
-            index: true,
-            sparse: true
-        }, 
-        githubConnected: {
-            type: Boolean,
-            default: false
-        },
-        githubAccessToken: {
-            type: String,
-            select: false
-        }, 
-        githubRefreshToken: {
-            type: String,
-            select: false
-        }, 
-        githubTokenIV: {
-            type: String,
-            select: false
-        }, 
-        githubTokenExpiry: {
-            type: Date
-        }, 
-        joinedDate: {
-            type: Date,
-            default: Date.now
-        }, 
-    }, {
-        timestamps: true, 
-        discriminatorKey: 'userType', 
-    });
+    githubUsername: {
+        type: String,
+        index: true,
+        sparse: true
+    }, 
+    githubId: {
+        type: String,
+        index: true,
+        sparse: true
+    }, 
+    githubConnected: {
+        type: Boolean,
+        default: false
+    },
+    githubAccessToken: {
+        type: String,
+        select: false
+    }, 
+    githubRefreshToken: {
+        type: String,
+        select: false
+    }, 
+    githubTokenIV: {
+        type: String,
+        select: false
+    }, 
+    githubTokenExpiry: {
+        type: Date
+    }, 
+    joinedDate: {
+        type: Date,
+        default: Date.now
+    }, 
+}, {
+    timestamps: true, 
+    discriminatorKey: 'userType', 
+});
 
-    // Base-model
-    const User = mongoose.model("User", userSchema);
+// Base-model
+const User = mongoose.model("User", userSchema);
 
-    const BountyHunter = User.discriminator("BountyHunter", new Schema({
-        skills: {
-            type: [String],
-            default: []
-        }, 
-        bountiesParticipatedIn: {
-            type: [String],
-            default: []
-        }, 
-        bountiesWon: {
-            type: [String],
-            default: []
-        }, 
-        totalAmountWon: {
-            type: Number,
-            default: 0
-        }, 
-        activeBountySubmissions: {
-            type: [String],
-            default: []
-        }, 
-    }));
-
-    const BountyProvider = User.discriminator("BountyProvider", new Schema({
-        organizationName: {
-            type: String,
-            default: null
-        }, 
-        organizationId: {
-            type: String,
-            default: null
-        },
-        organizationVerified: {
-            type: Boolean,
-            default: false
-        }, 
-        verificationDate: {
-            type: Date,
-            default: null
-        }, 
-        organizationWebsite: {
-            type: String,
-            default: null
-        }, 
-        userRoleInOrganization: {
-            type: String,
-            default: null
-        }, 
-        repositories: {
-            type: [{
-                name: String,        
-                fullName: String,     
-                description: String,  
-                url: String,          
-                isPrivate: Boolean,   
-                stars: Number,        
-                lastVerified: Date    
-            }],
-            default: []
-        }, 
-        bountiesListed: {
+const BountyHunter = User.discriminator("BountyHunter", new Schema({
+    skills: {
+        type: [String],
+        default: []
+    }, 
+    bountiesParticipatedIn: {
+        type: [String],
+        default: []
+    }, 
+    bountiesWon: {
+        type: [String],
+        default: []
+    }, 
+    totalAmountWon: {
         type: Number,
         default: 0
-        },
-        totalAmountDistributed: {
-            type: Number,
-            default: 0
-        }, 
-    }));
+    }, 
+    activeBountySubmissions: {
+        type: [String],
+        default: []
+    }, 
+}));
 
-    module.exports = { User, BountyHunter, BountyProvider };
+const BountyProvider = User.discriminator("BountyProvider", new Schema({
+    organizationName: {
+        type: String,
+        default: null
+    }, 
+    organizationId: {
+        type: String,
+        default: null
+    },
+    organizationVerified: {
+        type: Boolean,
+        default: false
+    }, 
+    verificationDate: {
+        type: Date,
+        default: null
+    }, 
+    organizationWebsite: {
+        type: String,
+        default: null
+    }, 
+    userRoleInOrganization: {
+        type: String,
+        default: null
+    }, 
+    repositories: {
+        type: [{
+            name: String,        
+            fullName: String,     
+            description: String,  
+            url: String,          
+            isPrivate: Boolean,   
+            stars: Number,        
+            lastVerified: Date    
+        }],
+        default: []
+    }, 
+    bountiesListed: {
+    type: Number,
+    default: 0
+    },
+    totalAmountDistributed: {
+        type: Number,
+        default: 0
+    }, 
+}));
+
+module.exports = { User, BountyHunter, BountyProvider };
